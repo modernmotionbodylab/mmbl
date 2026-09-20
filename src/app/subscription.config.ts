5b443222cb11374743d538d8a15bef200ec52315
robinyUArizona <robiny@arizona.edu>
2026-09-19 21:19:49 -0500

    Connect online and in-person Stripe subscriptions

--- src/app/subscription.config.ts
+++ src/app/subscription.config.ts
@@ -1,2 +1,5 @@
 // Public checkout URLs. Empty values display Coming soon instead of a broken link.
-export const subscriptionConfig = { online: '', inPerson: '' };
+export const subscriptionConfig = {
+  online: 'https://buy.stripe.com/aFaaEYehn2BTchy15y1Fe03',
+  inPerson: 'https://buy.stripe.com/fZu3cw5KR4K1bduaG81Fe02',
+};
